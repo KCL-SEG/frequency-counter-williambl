036f5d2c3ee809c32a432dcf355e362977c9c21e
williambl <william@williambl.com>
2022-10-06 11:36:35 +0100

    feat: frequencies function

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,6 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+    items_str = [str(i) for i in items]
+    frequencies = {k: items_str.count(k) for k in items_str}
     return frequencies
